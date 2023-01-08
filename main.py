2be8ded23d8f54de00ccd60d13fd7179c0764fe0
Maria Kokoeva <mrkokoeva@edu.hse.ru>
2023-01-08 17:59:49 +0300

    PEP8

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import sys
 
-def levin(a, b):
+
+def levinstein(a, b):
     n, m = len(a), len(b)
     cur_row = range(n + 1)
     for i in range(1, m + 1):
         old_row, cur_row = cur_row, [i] + [0] * n
         for j in range(1, n + 1):
+            add = old_row[j] + 1
+            change = old_row[j - 1]
+            delete = cur_row[j - 1] + 1
             if a[j - 1] != b[i - 1]:
-                cur_row[j] = min(old_row[j] + 1, cur_row[j - 1] + 1, old_row[j - 1] + 1)
+                cur_row[j] = min(add, delete, change + 1)
             else:
-                cur_row[j] = min(old_row[j] + 1, cur_row[j - 1] + 1, old_row[j - 1])
+                cur_row[j] = min(add, delete, change)
     return cur_row[n]
 
 
@@ -17,14 +21,9 @@ input_name = sys.argv[1]
 output_name = sys.argv[2]
 inp = open(input_name, 'r')
 sc = open(output_name, 'w')
-sp = inp.read().split('\n')
+lst = inp.read().split('\n')
 answers = []
-for i in range(0, len(sp), 2):
-    answers.append(str(levin(sp[i], sp[i + 1])))
+for i in range(0, len(lst), 2):
+    answers.append(str(levinstein(lst[i], lst[i + 1])))
 new_ans = "\n".join(answers)
 sc.write(new_ans)
-
-
-
-
-
